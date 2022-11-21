d00241d41227a6cd22a75a18ef89b88db8e0728f
savvas-paragkamian <s.paragkamian@gmail.com>
2022-11-21 20:21:52 +0200

    parse worms data and connect to REST API to get attributes

---
+++ scripts/get_worms_traits.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import requests, sys, json, time
+from datetime import date
+
+worms_url = "https://www.marinespecies.org/rest/AphiaAttributesByAphiaID/"
+
+taxa = []
+
+with open("../WoRMS_2022-04-01/taxon.txt") as file:
+    for line in file:
+        
+        columns = []
+        l=line.split('\t')
+        
+        aphia_url=l[26].split('=')
+        
+        if (len(aphia_url)>1):
+            aphia_id=aphia_url[2]
+        else:
+            aphia_id=l[26]
+
+        columns = [l[5], l[19], aphia_id]
+        taxa.append(columns)
+
+l = taxa[222]
+aphia_id = l[2]
+print(aphia_id)
+url = worms_url + aphia_id
+aphia_attr = requests.get(url = url)
+time.sleep(3)
+print(aphia_attr.status_code)
+print(aphia_attr.json())

978debcb2d3c38a243b1683e40c6d2757077c1a6
s.paragkamian <s.paragkamian@hcmr.gr>
2023-03-21 12:31:09 +0200

    initiate the parsing of json

---
+++ scripts/parse_worms_traits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+###############################################################################
+# script name: parse_worms_traits.py
+# developed by: Savvas Paragkamian
+# framework: EMODnet Biology Phase IV
+###############################################################################
+# GOAL:
+# Aim of this script is parse the json files retrieved from worms traits and 
+# create a tsv file
+###############################################################################
+# usage:./parse_worms_traits.py
+###############################################################################
+
+import sys, json
+
+

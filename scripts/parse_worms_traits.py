cd75b95b1ccc733cd7b63900eb6fd1abc286cb32
savvas-paragkamian <s.paragkamian@gmail.com>
2023-09-14 19:53:59 +0300

    flatten json, i have to print the data

--- scripts/parse_worms_traits.py
+++ scripts/parse_worms_traits.py
@@ -15,10 +15,47 @@ import pandas as pd
 import json
 import os
 import sys
+import re
 
+def flatten_json(nested_json, separator='|'):
+    flat_dict = {}
+
+    def flatten(x, name=''):
+        if type(x) is dict:
+            for a in x:
+                flatten(x[a], name + a + separator)
+        elif type(x) is list:
+            i = 0
+            for a in x:
+                flatten(a, name + str(i) + separator)
+                i += 1
+        else:
+            flat_dict[name[:-1]] = x
+
+    flatten(nested_json)
+    return flat_dict
+
+# Example usage:
+
+js = 'worms_traits/traits_360492.json'
+with open(js) as json_file:
+	data = json.load(json_file)
+
+flat_dict = flatten_json(data)
+
+df = pd.json_normalize(flat_dict)
+
+for i in flat_dict:
+    name = re.sub("children|0|","",i)
+    d = name.split("|")
+    print(d)
+    print(name,"\t",flat_dict[i])
+
+#print(flat_dict)
 directory_path = 'worms_traits/'
 print(f'loading all json files from the {directory_path}')
 
+sys.exit()
 # Define the output TSV file path
 output_file = 'output.tsv'
 output_directory = 'worms_traits_df/'
@@ -41,20 +78,8 @@ print(f'the number of files read are : {len(data_list)}')
 dataframes = []
 
 for json_obj in data_list:
-    df = pd.json_normalize(data,
-                           record_path=['children'],
-                           meta=['AphiaID',
-                                 'AphiaID_Inherited',
-                                 'reference',
-                                 'qualitystatus',
-                                 'measurementType',
-                                 'CategoryID',
-                                 'source_id',
-                                 'measurementValue',
-                                 'measurementTypeID'])
-
+    df = pd.json_normalize(json_obj, record_path=['children'], meta=['AphiaID', 'AphiaID_Inherited', 'reference', 'qualitystatus', 'measurementType', 'CategoryID', 'source_id', 'measurementValue', 'measurementTypeID'], record_prefix='children.', meta_prefix='meta.')
     dataframes.append(df)
-
 
 # Concatenate the list of DataFrames into a single DataFrame
 combined_df = pd.concat(dataframes, ignore_index=True)
